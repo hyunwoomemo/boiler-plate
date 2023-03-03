1edea5b4cf4c83af3b233309df379b954a34ebc7
hyunwoomemo <archihw94@gmail.com>
2023-03-04 01:06:08 +0900

    first commit

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1
+  },
+  password: {
+    type: String,
+    minlength: 5
+  },
+  lastname: {
+    type: String,
+    maxlength: 50
+  },
+  role: {
+    // ex - 0이면 관리자 1이면 유저라고 지정할 수 있음
+    type: Number,
+    default: 0
+  },
+  image: String,
+  token: {
+    type: String
+  },
+  tokenExp: {
+    type: Number
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
